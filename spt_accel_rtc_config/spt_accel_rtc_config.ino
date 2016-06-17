f80a3fea9aec9b12a01e3350d96d38eb39fb9f67
George White <george@cantina.co>
2016-06-17 11:18:08 -0400

    Fix bug in RTC setup firmware

--- spt_accel_rtc_config/spt_accel_rtc_config.ino
+++ spt_accel_rtc_config/spt_accel_rtc_config.ino
@@ -6,16 +6,22 @@ RTC_DS1307 RTC;
 void setup() {
   Serial.begin(115200);
   
+  RTC.begin();
   if (!RTC.isrunning()) {
     Serial.println("RTC is not running");
     RTC.adjust(DateTime(__DATE__, __TIME__));
-    Serial.println("Time is now set, and clock is running");
-  }    
+  }
+  Serial.println("Time is now set, and clock is running");
 }
 
 void loop() {
+  printTime();
+}
+
+void printTime() {
   DateTime now = RTC.now();
   Serial.print("Unix Timestamp: ");
-  Serial.println(now.unixtime()); 
-
+  Serial.println(now.unixtime());
+  delay(1000);
 }
+
